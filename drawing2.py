649bfab4b2a01f8a6af60fc4f9129b652c06c929
@mvtinchurin_1 <mvtinchurin_1@edu.hse.ru>
2024-03-22 10:59:02 +0300

    First adding

--- drawing2.py
+++ drawing2.py
@@ -4,16 +4,24 @@ import numpy as np
 
 def print_meanders(combination):
     cnt = -1
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 8))
     plt.xticks(np.arange(1, len(combination) + 1, 1))
+    ax = plt.gca()
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.set_ylim([-3, 3])
+    ax.spines['bottom'].set_position(('data', 0))
     for i in range(len(combination) - 1):
         a = min(combination[i], combination[i + 1])
         b = max(combination[i], combination[i + 1])
         x = np.linspace(a, b, num=100, endpoint=True)
-        plt.plot(x, ((((b - a) / 2) ** 2 - (x - (a + b) / 2) ** 2) ** (1 / 2)) * cnt)
+        plt.plot(x, ((((b - a)/2)**2 - (x - (a + b)/2)**2)**(1/2)) * cnt, color='g')
         cnt *= -1
     plt.show()
 
 
-combination = [5, 4, 3, 2, 1, 6]
+
+combination = [5, 6, 7, 4, 1, 2, 3, 8]
 print_meanders(combination)

22c09d5db4049eb6353e21cda0a2e4d7566e7b6a
Kirill Pavlov <kirill-pavlov3105@mail.ru>
2025-04-14 19:34:36 +0300

    Add info in readme, small fixes in files

--- count_meanders.py
+++ count_meanders.py
@@ -4,6 +4,4 @@ import functions
 n = int(input('Задайте размер поиска меандров:\n'))
 m = input('Введите любой символ для вывода меандров, и нажмите Enter чтобы получить просто количество:\n')
 
-mndrs = functions.Meanders(n)
-all_mndrs = mndrs.get_all_meanders(m)
-mndrs.get_meanders_info()
+functions.Meanders(n).get_meanders_info()
--- save_all_meanders_in_file.py
+++ save_all_meanders_in_file.py
@@ -4,7 +4,7 @@ import functions
 import os
 
 
-n = int(input('Задайте размер поиска меандров:'))
+n = int(input('Задайте размер поиска меандров:\n'))
 m = input('Введите любой символ для вывода меандров, и нажмите Enter чтобы получить просто количество:\n')
 mndrs = functions.Meanders(n)
 all_mndrs = mndrs.get_all_meanders(m)

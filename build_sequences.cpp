8314beafa49c0554627a2e5b1e78e4d65c9710c4
Kirill Pavlov <kirill-pavlov3105@mail.ru>
2025-04-19 17:20:10 +0300

    Добавил реализацию подсчета меандров на C++

--- build_sequences.cpp
+++ build_sequences.cpp
@@ -1,62 +1,7 @@
 #include "functions.h"
 #include <chrono>
 #include <string>
-#include <stdio.h>
-
-int global_answer = 0;
-
-
-void go_deep_to_build(int n, int is_even, std::vector<int> &visited, std::vector<std::vector<int> > &neighs,
- std::vector<int> &sequence, int step, bool is_do) {
-    if (step == n) {
-        ++global_answer;
-        if (is_do) {
-            printf("Найдена комбинация номер %d: ", global_answer);
-            for (int elem : sequence) {
-                printf("%d, ", elem);
-            }
-            printf("\n");
-            for (int i = 0; i < n; ++i) {
-                printf("-");
-            }
-            printf("\n");
-        }
-        return;
-    }
-    for (int i = is_even; i < n + 1; i += 2) {
-        if (visited[i] == 0) {
-            for (int j = i + 1; j < n + 1; ++j) {
-                if (visited[j] == 0) {
-                    neighs[i][j] = 1;
-                    neighs[j][i] = 1;
-                }
-            }
-            if (!check_noted(n, i, visited, neighs)) {
-                for (int j = i + 1; j < n + 1; ++j) {
-                    if (visited[j] == 0) {
-                        neighs[i][j] = 0;
-                        neighs[j][i] = 0;
-                    }
-                }
-                continue;
-            }
-                
-            visited[i] = 1;
-
-            sequence[step] = i;
-            go_deep_to_build(n, 3 - is_even, visited, neighs, sequence, step + 1, is_do);
-
-            for (int j = i + 1; j < n + 1; ++j) {
-                if (visited[j] == 0) {
-                    neighs[i][j] = 0;
-                    neighs[j][i] = 0;
-                }
-            }
-
-            visited[i] = 0;
-        }
-    }
-}
+#include <iostream>
 
 
 int main() {
@@ -68,42 +13,9 @@ int main() {
 
     std::cin >> x;
 
-    std::cout << "Введите букву \'w\' или \'W\' чтобы получить вывод последовательностей меандров:" << std::endl;
+    Meanders meanders(x);
 
-    std::cin >> type_output;
-
-    if (type_output == "W" || type_output == "w") {
-        is_do_out = true;
-    }
-
-    std::vector<std::vector<int> > X_all(x + 1);
-
-    for (int i = 0; i < x + 1; ++i) {
-        for (int j = 0; j < x + 1; ++j) {
-            X_all[i].push_back(0);
-        }
-    }
-
-    std::vector<int> empty_visited(x + 1);
-    std::vector<int> empty_vector(x);
-
-    auto start_point = std::chrono::system_clock::now();
-    go_deep_to_build(x, 1, empty_visited, X_all, empty_vector, 0, is_do_out);
-    std::string string_for_meanders1 = " найден ";
-    std::string string_for_meanders2 = " меандр";
-    if (global_answer % 10 > 4 || (4 < global_answer % 100 && global_answer % 100 < 21) || global_answer % 10 == 0) {
-        string_for_meanders1 = " найдено ";
-        string_for_meanders2 = " меандров";
-    } else if (global_answer % 10 > 1) {
-        string_for_meanders1 = " найдено ";
-        string_for_meanders2 = " меандра";
-    }
-    std::cout << "Для числа " << x << string_for_meanders1 << global_answer << string_for_meanders2 << std::endl;
-
-    auto end_point = std::chrono::system_clock::now();
-    std::chrono::duration<double> between = end_point - start_point;
-
-    std::cout << "Всего прошло: " << between.count() << std::endl;
+    meanders.get_meanders_info();
 
     return 0;
 }
--- functions.h
+++ functions.h
@@ -1,29 +1,146 @@
 #include <algorithm>
+#include <bitset>
 #include <iostream>
 #include <set>
 #include <vector>
+#include <cmath>
 
-bool check_noted(int n, int pos, const std::vector<int> &visited, const std::vector<std::vector<int> > &neighs) {
-    for (int i = 1; i < n + 1; ++i) {
-        if (visited[i] == 1) {
-            int count = 0;
-            for (int j = 1; j < n + 1; ++j) {
-                if (neighs[pos][j] == 1 && neighs[i][j] == 1) {
-                    ++count;
-                }
+template <class T>
+std::ostream& operator << (std::ostream& os, const std::vector<T>& v) 
+{
+    os << "[";
+    for (typename std::vector<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii)
+    {
+        os << *ii << ", ";
+    }
+    os << "end]";
+    return os;
+}
+
+
+// Размер меандров считается ограниченным 32
+class Meanders {
+    private:
+        std::vector<std::vector<int>> all_meanders_;
+        std::chrono::duration<double> speed_;
+        int n_;
+
+        void __go_deep_to_build(int is_even, std::vector<bool>& visited, std::set<int>& set_visited,
+            std::vector<std::bitset<32>>& neighs, std::vector<int>& to_answer, int depth) {
+            if (depth == n_) {
+                all_meanders_.push_back(to_answer);
+                return;
             }
-
-            if (neighs[pos][i] == 1) {
-                if (count % 2 != 0) {
-                    return false;
-                }
-            } else {
-                if (count % 2 != 1) {
-                    return false;
+            for (int i = is_even; i < n_ + 1; i += 2) {
+                if (!visited[i]) {
+                    for (int j = i + 1; j < n_ + 1; ++j) {
+                        if (visited[j] == 0) {
+                            neighs[i].set(j);
+                            neighs[j].set(i);
+                        }
+                    }
+                    if (!check_noted(i, set_visited, neighs)) {
+                        for (int j = i + 1; j < n_ + 1; ++j) {
+                            if (!visited[j]) {
+                                neighs[i].reset(j);
+                                neighs[j].reset(i);
+                            }
+                        }
+                        continue;
+                    }
+                    visited[i] = true;
+                    set_visited.insert(i);
+                    to_answer[depth] = i;
+                    __go_deep_to_build(3 - is_even, visited, set_visited, neighs, to_answer, depth + 1);
+                    to_answer[depth] = 0;
+                    for (int j = i + 1; j < n_ + 1; ++j) {
+                        if (!visited[j]) {
+                            neighs[i].reset(j);
+                            neighs[j].reset(i);
+                        }
+                    }
+                    visited[i] = false;
+                    set_visited.erase(i);
                 }
             }
         }
-    }
 
-    return true;
+        bool check_noted(int pos, std::set<int>& visited, std::vector<std::bitset<32>>& neighs) {
+            for (int x : visited) {
+                if (neighs[pos][x] == 1) {
+                    std::bitset<32> land = neighs[pos] & neighs[x];
+                    if (land.count() % 2 == 1) {
+                        return false;
+                    }
+                } else {
+                    std::bitset<32> land = neighs[pos] & neighs[x];
+                    if (land.count() % 2 == 0) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    public:
+        Meanders(int m);
+
+        std::vector<std::vector<int>> get_all_meanders();
+        void get_meanders_info();
+        void print_meanders();
+
+        void head(size_t m=10) {
+            for (int i = 0; i < std::min(all_meanders_.size(), m); ++i) {
+                std::cout << all_meanders_[i] << std::endl;
+            }
+        }
+
+        void tail(size_t m=10) {
+            for (int i = all_meanders_.size() - std::min(all_meanders_.size(), m); i < all_meanders_.size(); ++i) {
+                std::cout << all_meanders_[i] << std::endl;
+            }
+        }
+};
+
+
+Meanders::Meanders(const int m) {
+    n_ = m;
+    std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+    std::cout << "- Подождите, идет инициализация класса меандров -" << std::endl;
+    std::cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+    std::vector<bool> is_visited(n_ + 1);
+    std::set<int> set_visited;
+    std::vector<std::bitset<32>> neighs(n_ + 1);
+    std::vector<int> future_meander(n_ + 1);
+    auto start_point = std::chrono::system_clock::now();
+    __go_deep_to_build(1, is_visited, set_visited, neighs, future_meander, 0);
+    speed_ = std::chrono::system_clock::now() - start_point;
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+    std::cout << "- Инициализация класса завершена -" << std::endl;
+    std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+}
+
+std::vector<std::vector<int>> Meanders::get_all_meanders() {
+    return all_meanders_;
+}
+
+void Meanders::get_meanders_info() {
+    size_t answer = all_meanders_.size();
+    std::string string_for_meanders1 = " найден ";
+    std::string string_for_meanders2 = " меандр";
+    if (answer % 10 > 4 || (4 < answer % 100 && answer % 100 < 21)) {
+        string_for_meanders1 = " найдено ";
+        string_for_meanders2 = " меандров";
+    } else if (answer % 10 > 1) {
+        string_for_meanders1 = " найдено ";
+        string_for_meanders2 = " меандра";
+    }
+    std::cout << "Для числа " << n_ << string_for_meanders1 << answer << string_for_meanders2 << std::endl;
+    std::cout << "Затрачено времени: " << speed_.count() << std::endl;
+}
+
+void Meanders::print_meanders() {
+    for (std::vector<int> meander : all_meanders_) {
+        std::cout << meander << std::endl;
+    }
 }
